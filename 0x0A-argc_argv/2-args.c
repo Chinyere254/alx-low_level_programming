c47ea435ada7bc84c42dfc79c146070321a7339d
Chinyere254 <chinyerepeace254@gmail.com>
2022-10-01 03:13:36 +0000

    1 or 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments to a function
+ * main - prints all the arguments it receives
  *
  * @argc: number of arguments
  * @argv: 1-D array of arguments
@@ -9,11 +9,16 @@
  * Return: Always 0
  */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	if (argc >= 1)
 	{
-		printf("%d\n", argc - 1);
+		int i;
+
+		for (i = 0; i < argc; i++)
+		{
+			printf("%s\n", argv[i]);
+		}
 	}
 
 	return (0);
