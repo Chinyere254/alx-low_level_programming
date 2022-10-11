94c87674939aaca1c92fd53f001065b40cabf7ba
Chinyere254 <chinyerepeace254@gmail.com>
2022-10-11 21:44:28 +0000

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,21 +1,25 @@
 #include "dog.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * print_dog - prints a struct dog
- * d: dog struct variable argument to print
- */
-
+ * @d: pointer to an element of type dog
+ * Return: nothing
+ **/
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		(d->name == NULL) ? printf("Name: (nil)\n") :
-			printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		(d->owner == NULL) ? printf("Owner: (nil)\n") :
-			printf("Owner: %s\n", d->owner);
-	}
+	if (d == NULL)
+		;
 	else
-		return;
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %f\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+																	}
 }
---
+++ 0x0E-structures_typedef/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include "dog.h"
+
+
+
+/**
+ *  *
+ *   *  * main - check the code
+ *    *
+ *     *   *
+ *      *
+ *       *    * Return: Always 0.
+ *        *
+ *         *     */
+
+int main(void)
+
+{
+
+		    struct dog my_dog;
+
+		    		init_dog(&my_dog, "Pooppy", 3.5, "Bob");
+
+							 
+
+							        return (0);
+
+}
